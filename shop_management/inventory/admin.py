457612ec21c40b8d599d3c015a6049ca9cc919a6
Dawit Belayneh <dawitbelayneh9@gmail.com>
2025-10-09 23:30:17 -0900

    add admin

--- shop_management/inventory/admin.py
+++ shop_management/inventory/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from .models import User, Category, Product, Customer, Sale, SaleItem
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(Sale)
+admin.site.register(SaleItem)

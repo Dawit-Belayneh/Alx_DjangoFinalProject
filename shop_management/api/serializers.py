32cbfd93cfbcb095287e9dae8fe614908d657475
Dawit Belayneh <dawitbelayneh9@gmail.com>
2025-10-16 02:01:43 -0900

    add all serializers class

--- shop_management/api/serializers.py
+++ shop_management/api/serializers.py
@@ -1,8 +1,34 @@
 from rest_framework import serializers
-from inventory.models import User
+from inventory.models import User, Category, Product, Customer, Sale, SaleItem
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id','username','role']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+
+class SaleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sale
+        fields = '__all__'
+
+class SaleItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SaleItem
+        fields = '__all__'

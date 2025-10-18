fa3f6b3c515ef137e3d1bc163e0810e6130ffe87
Dawit Belayneh <dawitbelayneh9@gmail.com>
2025-10-18 00:54:19 -0900

    finish authentication and permission

--- shop_management/api/urls.py
+++ shop_management/api/urls.py
@@ -4,16 +4,18 @@ from . import views
 
 
 urlpatterns = [
+    path("signup/", views.SignupAPIView.as_view(), name="signup"),
+    path("login/", views.LoginAPIView.as_view(), name="login"),
     path("users/", views.UserListCreateAPIView.as_view(), name="api-user"),
-    path("user/<int:pk>", views.UserDetailAPIView.as_view(), name="api-user-detail"),
+    path("users/<int:pk>", views.UserDetailAPIView.as_view(), name="api-user-detail"),
     path("categorys/", views.CategoryListCrateAPIView.as_view(), name="api-category"),
-    path("category/<int:pk>", views.CategoryDetailAPIView.as_view(), name="api-category-detail"),
+    path("categorys/<int:pk>", views.CategoryDetailAPIView.as_view(), name="api-category-detail"),
     path("products/", views.ProductListCrateAPIView.as_view(), name ="api-product"),
-    path("product/<int:pk>/", views.ProductDetailAPIView.as_view(), name="api-product-detail"),
+    path("products/<int:pk>/", views.ProductDetailAPIView.as_view(), name="api-product-detail"),
     path("sales/", views.SaleListCreateAPIView.as_view(), name = "api-sales"),
-    path("sale/<int:pk>/", views.SaleDetailAPIView.as_view(), name="api-sales-detail"),
+    path("sales/<int:pk>/", views.SaleDetailAPIView.as_view(), name="api-sales-detail"),
     path("saleitems/", views.SaleItemListCreateAPIView.as_view(), name = "api-saleItem"),
-    path("saleitem/<int:pk>/", views.SaleItemDetailAPIView.as_view(), name = "api-saleitem-detail"),
+    path("saleitems/<int:pk>/", views.SaleItemDetailAPIView.as_view(), name = "api-saleitem-detail"),
     path("customers/", views.CustomerListCreateAPIView.as_view(), name="api-customer"),
-    path("customer/<int:pk>/", views.CustomerDetailAPIView.as_view(), name="api-customer-detail"),
+    path("customers/<int:pk>/", views.CustomerDetailAPIView.as_view(), name="api-customer-detail"),
 ]
